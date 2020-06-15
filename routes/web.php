2638d92130b9a895555aca0ca4e55714f92c3338
Triosandriawan <65974603+Triosandriawan@users.noreply.github.com>
2020-06-15 22:04:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,9 @@ Route::patch('/produk/{product}','ProductsController@update');
 Route::get('/produk/{product}/edit','ProductsController@edit');
 Auth::routes();
 
+Route::get('/', 'CartController@shop')->name('shop');
+Route::get('/cart', 'CartController@cart')->name('cart.index');
+Route::post('/add', 'CartController@add')->name('cart.store');
+Route::post('/update', 'CartController@update')->name('cart.update');
+Route::post('/remove', 'CartController@remove')->name('cart.remove');
+Route::post('/clear', 'CartController@clear')->name('cart.clear');
